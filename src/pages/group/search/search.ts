9e85f42cc8398e2ecabcaa7faca8c184a39af17f
eric1234463 <eric1234463@gmail.com>
2017-08-06 09:11:40 +0800

    add ios and fix config

--- src/pages/group/search/search.ts
+++ src/pages/group/search/search.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController , App} from 'ionic-angular';
-import { AngularFireDatabase , FirebaseListObservable} from 'angularfire2/database';
 
 @Component({
     selector: 'page-group-searh',
     templateUrl: 'search.html'
 })
 export class GroupSearchPage {
-    constructor(public navCtrl: NavController, public db: AngularFireDatabase, public appCtrl : App) {
+    constructor(public navCtrl: NavController, public appCtrl : App) {
         
     }
 
